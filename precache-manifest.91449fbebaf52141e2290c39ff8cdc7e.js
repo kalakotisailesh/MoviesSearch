11a694c8d2ecec12f0c1a2eaea289eb72c5c8524
sailesh <saileshkalakoti>
2019-07-05 11:44:22 +0530

    Updates

---
+++ precache-manifest.91449fbebaf52141e2290c39ff8cdc7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44dcfe5ac34e640342e0997402c22254",
+    "url": "/MoviesSearch/index.html"
+  },
+  {
+    "revision": "f03c293945ca15baffb6",
+    "url": "/MoviesSearch/static/css/main.777ef555.chunk.css"
+  },
+  {
+    "revision": "dc6f74c7cec37690c107",
+    "url": "/MoviesSearch/static/js/2.dc1c9c96.chunk.js"
+  },
+  {
+    "revision": "f03c293945ca15baffb6",
+    "url": "/MoviesSearch/static/js/main.95e6365a.chunk.js"
+  },
+  {
+    "revision": "1fa6ca12291aa9b5cde1",
+    "url": "/MoviesSearch/static/js/runtime~main.972018ef.js"
+  },
+  {
+    "revision": "a567c2208beec98f3d27a442304a3033",
+    "url": "/MoviesSearch/static/media/icon.a567c220.svg"
+  }
+]);
